9af866ebd5432e3e77c3ab44bfc18ef55c4fdeae
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2016-09-15 01:38:27 +0200

    Correct and add new specs

--- checkout.spec.js
+++ checkout.spec.js
@@ -14,51 +14,66 @@ describe("Checkout", () => {
     })
 
     // Public Methods
-    it("should expose a chainable .scan() method", () => {
+    it("should expose a .scan() method", () => {
         expect(typeof co.scan).toBe("function")
-        expect(co.scan()).toBe(co)
     })
+    describe(".scan()", () => {
+        it("should be chainable", () => {
+            expect(co.scan("VOUCHER")).toBe(co)
+        })
+    })
+
     it("should expose a .total() method", () => {
         expect(typeof co.total).toBe("function")
     })
+    describe(".total()", () => {
+        it("should return a string", () => {
+            expect(typeof co.total()).toBe("string")
+        })
+        it("should return a string in currency format (€)", () => {
+            expect(co.total()).toMatch(/^(0|[1-9][0-9]?)[.][0-9]{2}€$/)
+        })
+    })
 
     // Examples
-    it("should return 32.50€ when scanning VOUCHER, TSHIRT, MUG", () => {
-        co
-            .scan("VOUCHER")
-            .scan("TSHIRT")
-            .scan("MUG")
+    describe("Examples", () => {
+        it("should return 32.50€ when scanning VOUCHER, TSHIRT, MUG", () => {
+            co
+                .scan("VOUCHER")
+                .scan("TSHIRT")
+                .scan("MUG")
 
-        expect(co.total()).toBe("32.50€")
-    })
-    it("should return 25.00€ when scanning VOUCHER, TSHIRT, VOUCHER", () => {
-        co
-            .scan("VOUCHER")
-            .scan("TSHIRT")
-            .scan("VOUCHER")
+            expect(co.total()).toBe("32.50€")
+        })
+        it("should return 25.00€ when scanning VOUCHER, TSHIRT, VOUCHER", () => {
+            co
+                .scan("VOUCHER")
+                .scan("TSHIRT")
+                .scan("VOUCHER")
 
-        expect(co.total()).toBe("25.00€")
-    })
-    it("should return 81.00€ when scanning TSHIRT, TSHIRT, TSHIRT, VOUCHER, TSHIRT", () => {
-        co
-            .scan("TSHIRT")
-            .scan("TSHIRT")
-            .scan("TSHIRT")
-            .scan("VOUCHER")
-            .scan("TSHIRT")
+            expect(co.total()).toBe("25.00€")
+        })
+        it("should return 81.00€ when scanning TSHIRT, TSHIRT, TSHIRT, VOUCHER, TSHIRT", () => {
+            co
+                .scan("TSHIRT")
+                .scan("TSHIRT")
+                .scan("TSHIRT")
+                .scan("VOUCHER")
+                .scan("TSHIRT")
 
-        expect(co.total()).toBe("81.00€")
-    })
-    it("should return 74.50€ when scanning VOUCHER, TSHIRT, VOUCHER, VOUCHER, MUG, TSHIRT, TSHIRT", () => {
-        co
-            .scan("VOUCHER")
-            .scan("TSHIRT")
-            .scan("VOUCHER")
-            .scan("VOUCHER")
-            .scan("MUG")
-            .scan("TSHIRT")
-            .scan("TSHIRT")
+            expect(co.total()).toBe("81.00€")
+        })
+        it("should return 74.50€ when scanning VOUCHER, TSHIRT, VOUCHER, VOUCHER, MUG, TSHIRT, TSHIRT", () => {
+            co
+                .scan("VOUCHER")
+                .scan("TSHIRT")
+                .scan("VOUCHER")
+                .scan("VOUCHER")
+                .scan("MUG")
+                .scan("TSHIRT")
+                .scan("TSHIRT")
 
-        expect(co.total()).toBe("74.50€")
+            expect(co.total()).toBe("74.50€")
+        })
     })
 })
